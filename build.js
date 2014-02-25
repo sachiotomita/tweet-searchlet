506fbdc984067b5b283c469de842d4005c320049
MakeNowJust <makenowjust@myopera.com>
2014-02-25 17:32:36 +0900

    beginning of the searchlet history

---
+++ build.js
@@ -0,0 +1,147 @@
+//requires
+var
+fs       = require('fs'),
+qs       = require('qs'),
+async    = require('async'),
+_        = require('lodash'),
+prompt   = require('prompt').start(),
+uglify   = require('uglify-js'),
+ntwitter = require('ntwitter');
+
+//constants
+var
+TWEET_JS = './tweet.js',
+LIBS = ['sha1.js', 'OAuth.js'];
+
+//processing command line arguments
+var
+program = require('commander');
+
+program
+  .version('0.1.0')
+  .option('-i, --input-pattern <pattern>', 'string to replace INPUT_PATTERN of tweet.js (default: "%s")', String)
+  .option('-o, --output <filename>', 'output filename (default: tweet.<screen_name>.searchlet.js)', String)
+  .option('-c, --config <filename>', 'configuration filename (default: ./config.json)', String)
+  .parse(process.argv);
+
+_.defaults(program, {
+  inputPattern: '"%s"',
+  output: 'tweet.<screen_name>.js',
+  config: './config.json',
+});
+
+//main
+var
+config = require(program.config);
+
+async.waterfall([
+  function checkAuthorized(next) {
+    //check authorized
+    if (config.access_token_key && config.access_token_secret) {
+      //already authorized
+      next(null);
+    } else {
+      //no authorized
+      var
+      twitter = new ntwitter(config);
+      
+      //start authorization
+      async.waterfall([
+        function getRequestToken(next) {
+          twitter.oauth.getOAuthRequestToken(next);
+        },
+        function promptPin(token, tokenSecret, params, next) {
+          console.log('authorize on browser at\n  %s', twitter.options.authorize_url + '?' + qs.stringify({oauth_token: token}));
+          
+          prompt.get(['pin'], function () {
+            next.apply(this, _.toArray(arguments).concat(token, tokenSecret));
+          });
+        },
+        function getAccessToken(input, token, tokenSecret, next) {
+          twitter.oauth.getOAuthAccessToken(token, tokenSecret, input.pin, next);
+        },
+        function setConfig(access_token_key, access_token_secret, params, next) {
+          config.access_token_key    = access_token_key;
+          config.access_token_secret = access_token_secret;
+          
+          next(null);
+        },
+      ], next);
+    }
+  },
+  
+  function getScreenName(next) {
+    var
+    twitter = new ntwitter(config);
+    
+    async.waterfall([
+      function verifyCredentials(next) {
+        twitter.verifyCredentials(next);
+      },
+      function pluckScreenName(res, next) {
+        var
+        screenName = res.screen_name;
+        program.output = program.output.replace('<screen_name>', screenName)
+        
+        next(null);
+      },
+    ], next);
+  },
+  
+  function readFiles(next) {
+    var
+    readFile = _.curry(fs.readFile),
+    tasks = { tweetJS: readFile(TWEET_JS, 'utf8') };
+    
+    LIBS.forEach(function (lib) {
+      tasks[lib] = readFile('lib/' + lib, 'utf8');
+    });
+    
+    async.parallel(tasks, next);
+  },
+  
+  function concatFiles(files, next) {
+    var
+    src = '(function (_OAuth) {\n';
+    src += files.tweetJS;
+    src += '\n})(function () {\n';
+    
+    LIBS.forEach(function (lib) {
+      src += files[lib] + '\n';
+    });
+    
+    src += 'return OAuth;\n';
+    src += '});';
+    
+    next(null, src);
+  },
+  
+  function compressTweetJS(src, next) {
+    var
+    ast = uglify.parse(src);
+    
+    ast.figure_out_scope();
+    ast.compute_char_frequency();
+    ast.mangle_names();
+    
+    next(null, ast.print_to_string());
+  },
+  
+  function replaceTweetJS(compressedSrc, next) {
+    compressedSrc = compressedSrc.replace(/%/g, ' % '); //escape for searchlet
+    compressedSrc = compressedSrc.replace('$input_pattern$', program.inputPattern);
+    _.each(config, function (value, key) {
+      compressedSrc = compressedSrc.replace('$' + key + '$', JSON.stringify(value));
+    });
+    next(null, compressedSrc);
+  },
+  
+  function writeTweetCustomJS(replacedSrc, next) {
+    replacedSrc = 'javascript:' + replacedSrc; //append "javascript:" pseudo scheme
+    fs.writeFile(program.output, replacedSrc, 'utf8', next);
+  },
+], function (err) {
+  if (err) return console.error(err);
+  
+  console.log('successed to generate=> %s', program.output);
+});
---
+++ tweet.js
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2014, Tuyusato Kitsune (@make_now_just).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * This script is template of seachlet. So, this script cannot run for alone.
+ *
+ * Please executing:
+ *
+ *     $ node build.js
+ */
+
+var
+input = $input_pattern$;
+
+var
+global = ('global', eval)('this'), //global eval magic!
+OAuth = global.OAuth;
+//when OAuth.js didn't loaded, load OAuth.js
+if (typeof global.OAuth === 'undefined') OAuth = global.OAuth = _OAuth();
+
+var
+base = 'https://api.twitter.com/1.1',
+api = '/statuses/update',
+url = base + api + '.json',
+parameters = [
+  ['status', input],
+],
+
+accessor = {
+  consumerKey:    $consumer_key$,
+  consumerSecret: $consumer_secret$,
+  token:          $access_token_key$,
+  tokenSecret:    $access_token_secret$,
+},
+message = {
+  method: 'POST',
+  action: url,
+  parameters: parameters,
+};
+
+//complete parameters for OAuth
+OAuth.completeRequest(message, accessor);
+
+var
+form = document.createElement('form'),
+ifrm = document.createElement('iframe'),
+name = 'tweet-on-location-bar-' + Date.now(), flag;
+
+//setting form
+form.action = message.action;
+form.method = message.method;
+form.target = name;
+
+//append parameters
+message.parameters.forEach(function (param) {
+  var
+  input = document.createElement('input');
+  
+  input.type = 'hidden';
+  input.name = param[0];
+  input.value = param[1];
+  
+  form.appendChild(input);
+});
+
+//setting iframe
+ifrm.name = name;
+ifrm.src = 'about:blank';
+
+flag = 0;
+ifrm.onload = function () {
+  if (flag++ === 0) { //load "about:blank"
+    setTimeout(function () { form.submit(); }, 0);
+  } else {            //load update.json
+    form.parentNode.removeChild(form);
+    ifrm.parentNode.removeChild(ifrm);
+  }
+};
+
+//append to html
+document.body.appendChild(form);
+document.body.appendChild(ifrm);
+
